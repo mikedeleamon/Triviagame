bba76e3f133e8f13937185f13906bc17a29d866e
mikedeleamon <ma3971a@student.american.edu>
2016-09-01 19:12:52 -0400

    added timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,68 @@
 // Trivia Game Js
 
+//watch
+window.onload = function(){
+    
+    
+    $('#reset').click(stopwatch.reset);
+    $('#start').click(stopwatch.start);
+};
+
+var stopwatch = {
+    time:11,
+    
+    reset: function () {
+        stopwatch.time = 0;
+        stopwatch.lap = 1;
+        //change the "display" div to "00:00"
+        $(".well").html("00:00")
+        //empty the "laps" div
+        
+    },
+    start: function(){
+        counter=setInterval(stopwatch.count,1000);
+        //Use setInterval to start the count here
+
+    },
+    stop: function(){
+        //Use clearInterval to stop the count here
+        clearInterval(counter);
+
+
+    },
+    
+    
+    count: function(){
+        stopwatch.time--;
+        //increment time by 1, remember we cant use "this" here
+        
+        //Get the current time, pass that into the stopwatch.timeConverter function, and save the result in a variable
+        realTime=stopwatch.timeConverter(stopwatch.time);
+        //Use the variable you just created to show the converted time in the "display" div
+        $(".well").html(realTime);
+    },
+    timeConverter: function(t){
+        //This function is done. You do not need to touch it. It takes the current time in seconds and converts it to minutes and seconds (mm:ss).
+        var minutes = Math.floor(t/60);
+        var seconds = t - (minutes * 60);
+        if (seconds < 10){
+            seconds = "0" + seconds;
+        }
+        if (minutes === 0){
+            minutes = "00";
+        } else if (minutes < 10){
+            minutes = "0" + minutes;
+        }
+
+        return minutes + ":" + seconds;
+    }
+};
+
+
+
+
+
+//Trivia
 var i = -1;
 var trivia = [
 //questions
@@ -30,6 +93,7 @@ $('#nextbtn').on('click',function(){
 		displayQuestion(i);
 	}
 })
+$('.answer').on('click', isRight())
 
 
 
@@ -37,11 +101,16 @@ $('#nextbtn').on('click',function(){
 // function displays queston
 
 function displayQuestion(i){
-	i++
+	i++;
 	$('#question').html("question "+i);
 	$('#qbody').html(trivia[0][i]);
 	console.log(trivia[0][i]);
 	showAnswer(i);
+	if (stopwatch.time==0) {
+		("#question").html("you ran out of time, press next for the next question")
+		
+		stopwatch.time=11;
+	}
 }
 
 // create a function to show the answers
